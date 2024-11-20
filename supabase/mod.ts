6d61f7f7c68001127084b40d09eb4fa1ec028d37
Roberto Villarreal <roberto.cesar136@hotmail.com>
2024-11-20 14:22:13 -0600

    cambios exportación

--- supabase/mod.ts
+++ supabase/mod.ts
@@ -1,25 +1,25 @@
 export {
-    headerMissingResponse as authFunc,
+    headerMissingResponse,
     tokenInvalidResponse,
     tokenMissingResponse,
 } from "./functions/general/auth_responses_functions.ts";
 export {
-    decode64 as resFunc,
+    decode64,
     errorResponse,
     response,
 } from "./functions/general/response_functions.ts";
 export {
     incorrectData,
     serverError,
-    successDataResponse as dataFunc,
+    successDataResponse,
     validateEmail,
     validateSingleData,
 } from "./functions/general/data_response_functions.ts";
-export { GeneralMessages as messages } from "./functions/general/global_messages.ts";
+export { GeneralMessages } from "./functions/general/global_messages.ts";
 
 export { Nullable } from "./functions/general/types.ts";
 
 export {
     mysqlConnection,
-    userDatabase as connection,
+    userDatabase,
 } from "./functions/mysql/connections.ts";
